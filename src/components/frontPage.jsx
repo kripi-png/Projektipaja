5e36882fdfcb029ef179c26b4aefd3018af50481
kripi__ <mainkriiper@gmail.com>
2020-09-02 16:12:42 +0300

    deleted and edited default files
    created scss/css files and folders

---
+++ src/components/frontPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class FrontPage extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>Otsikko</h1>
+      </div>
+    );
+  }
+}

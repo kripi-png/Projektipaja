ad212db9388f17a9df06ccf397992630d204313b
Roope Sinisalo <mainkriiper@gmail.com>
2020-11-04 12:15:17 +0200

    fixed a background scaling issue

--- src/components/frontPage.jsx
+++ src/components/frontPage.jsx
@@ -3,7 +3,6 @@ import Button from './button';
 
 export default class FrontPage extends React.Component {
   render() {
-    alert("Kaikki näkemäsi on työn ja suunnittelun alla eikä mitään ole lyöty lukkoon.")
     return (
       <div className="main">
         <Button
@@ -11,7 +10,7 @@ export default class FrontPage extends React.Component {
           style={{left: "2%", top: "5%"}}
           type={'text'}
           hoverInfo={'Tekstiä'}
-          contentStyle={{ backgroundColor: '#820081', fontSize: "1.2em", padding: '.75em', border: "2px solid #000" }}
+          contentStyle={{ backgroundColor: '#820081', fontSize: "1.2em", padding: '.75em', border: "1px solid #000" }}
           content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
         />
         <Button
@@ -23,7 +22,7 @@ export default class FrontPage extends React.Component {
           content={'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'}
         />
         <Button
-          img={'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/YouTube_social_white_circle_%282017%29.svg/1024px-YouTube_social_white_circle_%282017%29.svg.png'}
+          img={'https://i.imgur.com/RgYSq9J.gif'}
           style={{left: "66.5%", top: "12.2%", transform: "translate(-50%, -50%)", width: "4em" }}
           contentStyle={{ margin: "0 0 0 -500px" }}
           type={'iframe'}
